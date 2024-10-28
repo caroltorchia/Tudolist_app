80b4393e7bbdf3989a109ae9e18f6e4e67840b6b
ANA CAROLINA TORCHIA DE SOUZA <95150688+caroltorchia@users.noreply.github.com>
2024-10-28 12:50:45 -0300

    Revert "Tela cadastro ajustes: front"
    This reverts commit 8813c7d81bda8af5299dfd77cbca51186f76640b.

--- register/forms.py
+++ register/forms.py
@@ -1,74 +1,16 @@
-"""summary
-
-    Raises:
-        forms.ValidationError: description
-
-    Returns:
-        type: description
-"""
-
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
 from .models import User
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(
-        max_length=30,
-        required=True,
-        label="Nome",
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Seu Nome"}
-        ),
-    )
-    last_name = forms.CharField(
-        max_length=30,
-        required=True,
-        label="Sobrenome",
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Seu Sobrenome"}
-        ),
-    )
-    email = forms.EmailField(
-        max_length=254,
-        required=True,
-        label="E-mail",
-        widget=forms.EmailInput(
-            attrs={"class": "form-control", "placeholder": "example@email.com"}
-        ),
-    )
-    instituicao = forms.CharField(
-        max_length=100,
-        required=False,
-        label="Instituição de Ensino",
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Instituição de Ensino"}
-        ),
-    )
-    password1 = forms.CharField(
-        label="Senha",
-        widget=forms.PasswordInput(
-            attrs={"class": "form-control", "placeholder": "****"}
-        ),
-    )
-    password2 = forms.CharField(
-        label="Confirme a Senha",
-        widget=forms.PasswordInput(
-            attrs={"class": "form-control", "placeholder": "****"}
-        ),
-    )
+    first_name = forms.CharField(max_length=30, required=True, label="Nome")
+    last_name = forms.CharField(max_length=30, required=True, label="Sobrenome")
+    email = forms.EmailField(max_length=254, required=True, label='Email válido')
+    instituicao = forms.CharField(max_length=100, required=False, label="Instituição de Ensino")
 
     class Meta:
         model = User
-        fields = [
-            "first_name",
-            "last_name",
-            "instituicao",
-            "email",
-            "password1",
-            "password2",
-        ]
+        fields = ['first_name', 'last_name', 'instituicao', 'email', 'password1', 'password2']
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -79,30 +21,24 @@ class RegisterForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        user.instituicao = self.cleaned_data["instituicao"]
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.instituicao = self.cleaned_data['instituicao']
         if commit:
             user.save()
         return user
 
-
 class LoginForm(AuthenticationForm):
-    username = forms.EmailField(
-        label="Email",
-        max_length=254,
-        widget=forms.EmailInput(
-            attrs={"class": "form-control", "placeholder": "example@email.com"}
-        ),
-    )
-    password = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(
-            attrs={"class": "form-control", "placeholder": "Senha"}
-        ),
-    )
+    username = forms.EmailField(label="Email", max_length=254, widget=forms.EmailInput(attrs={
+                'class': "form-control", 
+                'placeholder': 'email@example.com'
+                }))
+    password = forms.CharField(label="Senha", widget=forms.PasswordInput(attrs={
+                'class': "form-control",
+                'placeholder': 'Senha'
+                }))
 
     class Meta:
         model = User
-        fields = ["username", "password"]
+        fields = ['username', 'password']
--- register/views.py
+++ register/views.py
@@ -1,88 +1,100 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, login
+from .forms import RegisterForm, LoginForm
+from django.template import RequestContext
 from django.core.mail import send_mail
-from django.http import JsonResponse
-from django.shortcuts import redirect, render
 from django.utils.crypto import get_random_string
-
-from .forms import LoginForm, RegisterForm
+from django.contrib import messages
+from django.http import JsonResponse
+import json
 
 
 def index(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.username = form.cleaned_data["email"]
+            user.username = form.cleaned_data['email']  
             user.save()
-            request.session["user_email"] = user.email
-            return redirect("send_verification")
+            request.session['user_email'] = user.email
+            return redirect('enviar_codigo')
         else:
             print(form.errors)
     else:
         form = RegisterForm()
-    return render(request, "index.html", {"form": form})
+    return render(request, 'register/registro.html', {'form': form})
 
 
-def send_verification(request):
-    user_email = request.session.get("user_email", None)
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = form.cleaned_data['email']  
+            user.save()
+            request.session['user_email'] = user.email
+            return redirect('enviar_codigo')
+        else:
+            print(form.errors)
+    else:
+        form = RegisterForm()
+    return render(request, 'register/registro.html', {'form': form})
+
+def send_verification_code(request):
+    user_email = request.session.get('user_email', None)  
     if not user_email:
-        messages.error(request, "Endereço de e-mail não encontrado.")
-        return redirect("signup")
-
-    code = get_random_string(length=6, allowed_chars="0123456789")
-
-    request.session["verification_code"] = code
-
+        messages.error(request, 'Endereço de e-mail não encontrado.')
+        return redirect('registrar')
+    
+    code = get_random_string(length=6, allowed_chars='0123456789')
+    
+    request.session['verification_code'] = code
+    
     send_mail(
-        "Seu código de verificação",
-        f"Por favor, use o seguinte código para verificar sua conta: {code}",
-        "from@example.com",
+        'Seu código de verificação',
+        f'Por favor, use o seguinte código para verificar sua conta: {code}',
+        'from@example.com',
         [user_email],
         fail_silently=False,
     )
+    
+    return redirect('verificacao_de_codigo')
 
-    return redirect("confirm_verification")
-
-
-def confirm_verification(request):
-    user_email = request.session.get("user_email", None)
-    if request.method == "POST":
-        code_entered = "".join(request.POST.getlist("code"))
-        code_sent = request.session.get("verification_code")
-
+def confirm_code(request):
+    user_email = request.session.get('user_email', None) 
+    if request.method == 'POST':
+        code_entered = ''.join(request.POST.getlist('code'))
+        code_sent = request.session.get('verification_code')
+        
         if code_entered == code_sent:
-            messages.success(request, "E-mail verificado com sucesso!")
-            return JsonResponse({"success": True})
+            messages.success(request, 'E-mail verificado com sucesso!')
+            return JsonResponse({'success': True})
         else:
-            messages.error(request, "Código inválido. Tente novamente.")
-            return JsonResponse({"success": False})
-
-    return render(
-        request, "verification.html", {"user_email": user_email}
-    )
+            messages.error(request, 'Código inválido. Tente novamente.')
+            return JsonResponse({'success': False})
+    
+    return render(request, 'register/verificacao_de_codigo.html', {'user_email': user_email})
 
 
 def login_user(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = LoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST["username"]
-            password = request.POST["password"]
+            username = request.POST['username']
+            password = request.POST['password']
 
             user = authenticate(username=username, password=password)
             if user is not None:
                 if user.is_active:
-                    login(request, user)
-                    return redirect("dashboard")
+                  login(request, user)
+                  return redirect('dashboard')
             else:
-                return render(request, "login.html", {"form": form})
+                return render(request, 'register/login.html', {'form': form})
     else:
         form = LoginForm()
-
-    return render(request, "login.html", {"form": form})
-
+    
+    return render(request, 'register/login.html', {'form': form})
 
 def logout_user(request):
     logout(request)
-    return redirect("home")
+    return redirect('home')
