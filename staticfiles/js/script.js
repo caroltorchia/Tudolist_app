edc101de29bf0c4732f75a64029fb06b35c88e45
ANA CAROLINA TORCHIA DE SOUZA <95150688+caroltorchia@users.noreply.github.com>
2024-12-02 20:49:30 -0300

    chore: serving staticfiles

---
+++ staticfiles/js/script.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let timerElement = document.getElementById('timer');
+    let resendButton = document.getElementById('resendButton');
+    let timeLeft = 30;
+
+    let countdown = setInterval(function() {
+        if (timeLeft <= 0) {
+            clearInterval(countdown);
+            resendButton.disabled = false;
+            timerElement.innerText = '';
+        } else {
+            resendButton.disabled = true;
+            timeLeft -= 1;
+            timerElement.innerText = '00:' + (timeLeft < 10 ? '0' : '') + timeLeft;
+        }
+    }, 1000);
+
+
+    resendButton.addEventListener('click', function() {
+        timeLeft = 30;
+        resendButton.disabled = true;
+        countdown = setInterval(function() {
+            if (timeLeft <= 0) {
+                clearInterval(countdown);
+                resendButton.disabled = false;
+                timerElement.innerText = '';
+            } else {
+                timeLeft -= 1;
+                timerElement.innerText = '00:' + (timeLeft < 10 ? '0' : '') + timeLeft;
+            }
+        }, 1000);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let inputs = document.querySelectorAll('.code-inputs input');
+    inputs.forEach(function(input, index) {
+        input.addEventListener('input', function() {
+            if (input.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+    });
+});
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let code = '';
+    document.querySelectorAll('.code-inputs input').forEach(function(input) {
+        code += input.value;
+    });
+
+    document.getElementById('VerificationCodeInput').value = code;
+
+    // fetch(window.location.href, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+    //     },
+    //     body: JSON.stringify({ 'code': code })
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     if (data.success) {
+    //         alert('Verificação concluída!');
+    //         window.location.href = '/registrar/success/';
+    //     } else {
+    //         alert('Código inválido, tente novamente.');
+    //     }
+    // });
+    this.submit();
+});
